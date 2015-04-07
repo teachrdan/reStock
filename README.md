cff7f2feb58de5d232c2241e63de6fa4f50e7fe7
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-04-07 12:32:34 -0700

    Adds YQL, other initial functions.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # reStock
+
+Track the social media activity of top moving stocks.
+
+## Author
+
+Dan Tennery-Spalding
---
+++ reStock.js
@@ -0,0 +1,10 @@
+/*! reStock v0.0.0 - MIT license */
+'use strict';
+
+var reStock = function () {
+  // your code goes here
+}
+
+if ( typeof module !== "undefined" ) {
+  module.exports = reStock;
+}
