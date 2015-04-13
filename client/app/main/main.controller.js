8d96cbaffc1dead3b1b81bd9cb54bd63d6a22dde
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-04-13 11:53:21 -0700

    Saving companies in sector in tuples.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,14 +2,15 @@
 
 angular.module('reStockApp')
   .controller('MainCtrl', function ($scope, $http) {
+    $scope.rawData = [];
+    $scope.allStocks = [];
     $http.get('/api/yql').success(function(data) {
-      $scope.yqlData = data;
-    });
-
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+      angular.forEach(data.query.results.industry.company, function(company){
+        var tuple = [];
+        tuple[0] = company.name;
+        tuple[1] = company.symbol;
+        $scope.rawData.push(tuple);
+      });
     });
 
   });
--- server/routes.js
+++ server/routes.js
@@ -11,14 +11,15 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+
   app.get('/api/yql', function(req, res) {
     var yql = new yqlient.initYQL();
-    yql.query('select Symbol, Ask from yahoo.finance.quotes where
-    symbol in ("YHOO","AAPL","GOOG","MSFT")', function(err, data) {
-      if (err) { console.log(err); }
-      else { console.log(data); }
+    yql.query('select * from yahoo.finance.industry where id="112"', function(err, data) {
+      if (err) { console.log('yql error:', err); }
+      else {
+        res.send(data);
+      }
     });
-    // res.send(response from yql call)
   });
 
   // All undefined asset or api routes should return a 404
@@ -31,5 +32,3 @@ module.exports = function(app) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
 };
-
-// module.exports router?
